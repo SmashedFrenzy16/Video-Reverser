a4a1027da3f6cca883de1a92af10ae57f37f978d
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2024-09-17 10:22:28 +0100

    Update main.py with button and entry field

--- Video-Reverser/main.py
+++ Video-Reverser/main.py
@@ -15,4 +15,14 @@ blank_label = Label(root, text="")
 
 blank_label.pack()
 
+location_e = Entry(root, width=100, borderwidth=5)
+
+location_e.pack()
+
+location_e.insert(0, "Enter in the full video path that you want to reverse")
+
+location_button = Button(root, text="Enter", command=execute)
+
+location_button.pack()
+
 root.mainloop()
