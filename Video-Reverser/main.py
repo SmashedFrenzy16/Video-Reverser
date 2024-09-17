2441a263732b5b92197be17def845b9507c6b5cd
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2024-09-17 10:23:11 +0100

    Update main.py with main code for execution

--- Video-Reverser/main.py
+++ Video-Reverser/main.py
@@ -19,6 +19,37 @@ location_e = Entry(root, width=100, borderwidth=5)
 
 location_e.pack()
 
+def execute():
+
+    vid = cv2.VideoCapture(location_e.get())
+
+    check, video = vid.read()
+
+    frame_count = 0
+
+    check = True
+
+    frame_list = []
+
+    while check == True:
+
+        cv2.imshow(f"video_frame_{frame_count}", video)
+
+        check, video = vid.read()
+
+        frame_list.append(video)
+
+        counter += 1
+
+    frame_list.pop()
+
+    for i in frame_list:
+
+        cv2.imshow("Frame:", i)
+
+        if cv2.waitKey(25) and 0xFF == ord("q"): 
+            break
+
 location_e.insert(0, "Enter in the full video path that you want to reverse")
 
 location_button = Button(root, text="Enter", command=execute)
