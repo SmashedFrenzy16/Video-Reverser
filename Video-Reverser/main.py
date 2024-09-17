848812c01811716f8d2a4a1dfa48c6740825df5e
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2024-09-17 10:21:30 +0100

    Update main.py with basic structure

--- Video-Reverser/main.py
+++ Video-Reverser/main.py
@@ -1,2 +1,18 @@
 from tkinter import *
 import cv2
+
+root = Tk()
+
+root.title("Video Reverser")
+
+
+title_label = Label(
+            root, fg="black", text="Video Reverser", font=("Arial", 48))
+
+title_label.pack()
+
+blank_label = Label(root, text="")
+
+blank_label.pack()
+
+root.mainloop()
